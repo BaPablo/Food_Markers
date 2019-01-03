4d6df141878287341e983ddaba7432738775f4a0
Pablo Barría <pablobarria@imac-de-dciufro-4.local>
2019-01-03 12:16:35 -0300

    Initial Commit

---
+++ Food_MarkersTests/Food_MarkersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Food_MarkersTests.swift
+//  Food_MarkersTests
+//
+//  Created by Pablo Barría on 03-01-19.
+//  Copyright © 2019 Universidad de La Frontera. All rights reserved.
+//
+
+import XCTest
+@testable import Food_Markers
+
+class Food_MarkersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
