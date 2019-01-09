d0f79816770867b32914f2a94407bd94a32794a0
Pablo Barría <pablobarria@imac-de-dciufro-4.local>
2019-01-09 18:20:37 -0300

    3D Model

--- Food_Markers/ViewController.swift
+++ Food_Markers/ViewController.swift
@@ -13,39 +13,46 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    private var planeNode: SCNNode?
+    private var imageNode: SCNNode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-    /*  
-        // Create a new scene
-        let scene = SCNScene()
+
+//        sceneView.debugOptions = [.showFeaturePoints]
         
-        // Set the scene to the view
-        sceneView.scene = scene
-    */
-    
+//        let scene = SCNScene(named: "art.scnassets/apple.scn")!
+//
+       sceneView.automaticallyUpdatesLighting = true
+//
+//        sceneView.scene = scene
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+//        configuration.planeDetection = .horizontal
         
         //Busqueda del directorio con los marcadores a detectar
         guard let markers = ARReferenceImage.referenceImages(inGroupNamed: "Markers", bundle: nil)
             else {
                 fatalError("No se encuentra un directorio de marcadores asociado")
         }
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+
         
         // Se define donde estan guardados los marcadores
         configuration.detectionImages = markers
+        
                 
         // Run the view's session
-        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-    
+        sceneView.session.run(configuration)
     
     }
     
@@ -57,18 +64,64 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        //guard  else { return }
-        
         // Si el ancla que se crea es una imagen, significa que se reconocio uno de los marcadores
+//       guard let imageAnchor = anchor as? ARImageAnchor else {return}
+//        let planeScene = SCNScene(named: "art.scnassets/apple.scn")
+//        let planeNode = planeScene?.rootNode.childNodes.first
+//
+//        let (min,max) = (planeNode?.boundingBox)!
+//        let size = SCNVector3Make(max.x - min.x, max.y - min.y, max.z - min.z)
+//
+//        let widthRatio = Float(imageAnchor.referenceImage.physicalSize.width)/size.x
+//        let heightRatio = Float(imageAnchor.referenceImage.physicalSize.height)/size.z
+//
+//        let finalRatio = [widthRatio,heightRatio].min()!
+//
+//        planeNode?.transform = SCNMatrix4(imageAnchor.transform)
+//
+//        let appeareanceAction = SCNAction.scale(to: CGFloat(finalRatio), duration: 0.4)
+//        appeareanceAction.timingMode = .easeOut
+//
+//        planeNode?.scale = SCNVector3Make(0, 0, 0)
+//
+//        sceneView.scene.rootNode.addChildNode(planeNode!)
+//
+//        planeNode?.runAction(appeareanceAction)
+//
+//        self.planeNode = planeNode
+//        self.imageNode = node
+
         if anchor is ARImageAnchor {
-            print("Marcador encontrado")
             let imageAnchor = anchor as! ARImageAnchor
-            let appleScene = SCNScene(named: "art.scnassets/apple.scn")!
-            //withName = Identity  --> Name of the node 
-           if let appleNode = appleScene.rootNode.childNode(withName: "apple", recursively: true) {
-                appleNode?.position = SCNVector3(anchor.transform.columns.3.x,anchor.transform.columns.3.y + 0.1, anchor.transform.columns.3.z)
-                appleScene.scene.rootNode.addChildNode(appleNode)
-           }
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x:0, y: 0, z: -10)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-.pi/2, 1, 0, 0)
+            
+            let material = SCNMaterial()
+            material.diffuse.contents = UIColor.red
+            
+            plane.materials = [material]
+            
+            planeNode.geometry = plane
+            
+            node.addChildNode(planeNode)
+            
+            let appleScene = SCNScene(named: "art.scnassets/apple.scn")
+            
+            if let appleNode = appleScene?.rootNode.childNode(withName: "apple", recursively: true){
+                print("applenode")
+                // appleNode.position = SCNVector3(x:planeNode.position.x, y: planeNode.position.y+0.1, z:planeNode.position.z+0.1)
+                
+                //appleNode.scale = SCNVector3(x: 0.05, y: 0.05, z: 0.05)
+                sceneView.scene.rootNode.addChildNode(appleNode)
+            }
             
         }
+        
     }
+}
