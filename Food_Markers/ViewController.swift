a471e02cffd7a0e0b1bd270ebc4c23de3af95bda
Pablo Barría <pablobarria@imac-de-dciufro-4.local>
2019-01-22 13:02:34 -0300

    Falta por corregir la selección del botón  que se mantiene al cambiar el modelo

--- Food_Markers/ViewController.swift
+++ Food_Markers/ViewController.swift
@@ -20,37 +20,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBAction func sizeSelection(_ sender: UISegmentedControl) {
         print(sender.selectedSegmentIndex)
         var actualNode = SCNNode()
-        print("Cantidad de nodos: " )
-        print(sceneView.scene.rootNode.childNodes.count)
+        print("Cantidad de nodos:", sceneView.scene.rootNode.childNodes.count)
         for node in sceneView.scene.rootNode.childNodes {
-            print ("Debug description: " + node.debugDescription)
+            print ("Debug description: ", node.debugDescription)
+            print ("Node name: ", node.name)
+            print ("Node hidden: ", node.isHidden)
+            
+            if node.name == "fromAnchor" && !node.isHidden{
+                actualNode = (node.childNodes.first)!
+            }
         }
-        actualNode = sceneView.scene.rootNode.childNode(withName: "apple", recursively: true)!
-        var actualNodeScale = actualNode.scale
-        print(actualNode)
         switch sizeOptionsSegments.selectedSegmentIndex{
-        case 0:
-            actualNode.scale = SCNVector3(0.001, 0.001, 0.001)
-            sceneView.scene.rootNode.childNodes.last?.replaceChildNode(
-            sceneView.scene.rootNode.childNodes.last!, with: actualNode)
-        case 1:
-            print(sender.selectedSegmentIndex)
-            actualNode.scale = actualNodeScale
-            sceneView.scene.rootNode.childNodes.last?.replaceChildNode(
+            case 0:
+                actualNode.scale = SCNVector3(0.001, 0.001, 0.001)
+                sceneView.scene.rootNode.childNodes.last?.replaceChildNode(
                 sceneView.scene.rootNode.childNodes.last!, with: actualNode)
-        case 2:
-            print(sender.selectedSegmentIndex)
-        default:
-            print("mMM")
+            case 1:
+                print(sender.selectedSegmentIndex)
+                actualNode.scale = SCNVector3(0.005, 0.005, 0.005)
+                sceneView.scene.rootNode.childNodes.last?.replaceChildNode(
+                    sceneView.scene.rootNode.childNodes.last!, with: actualNode)
+            case 2:
+                actualNode.scale = SCNVector3(0.01, 0.01, 0.01)
+                sceneView.scene.rootNode.childNodes.last?.replaceChildNode(
+                    sceneView.scene.rootNode.childNodes.last!, with: actualNode)
+            default:
+                print("mMM")
         }
-        
     }
     
     override func viewDidLoad() {
         // Set the view's delegate
         sceneView.delegate = self
         sceneView.automaticallyUpdatesLighting = true
-        
+        sceneView.debugOptions = [.showBoundingBoxes]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -68,6 +71,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             configuration.trackingImages = markers
             // Run the view's session
             sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+            
         } else {
             let configuration = ARWorldTrackingConfiguration()
             configuration.detectionImages = markers
@@ -83,89 +87,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     //MARK: -ARSCNViewDelegate
-    
-
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode?{
 
         let node = SCNNode()
         if anchor is ARImageAnchor {
 
             if let imageAnchor = anchor as? ARImageAnchor{
-
-                    let nombreMarker = imageAnchor.referenceImage.name
-                    switch (nombreMarker) {
+                let nombreMarker = imageAnchor.referenceImage.name
+                switch (nombreMarker) {
                     case "apple":
                         print ("Apple marker detected")
                         let appleScene = SCNScene(named: "art.scnassets/apple/apple.scn")
                         if let appleNode = appleScene?.rootNode.childNodes.first{
                             appleNode.eulerAngles.x = .pi / 2
                             appleNode.name = nombreMarker
+                            appleNode.position = SCNVector3(0, 0.1, 0)
                             node.addChildNode(appleNode)
-                            appleNode.position = SCNVector3(0,0.2 ,0.01)
-                            self.sceneView.scene.rootNode.addChildNode(node)
                         }
-                    
-                    case "meat":
+                    case "stark":
                         let markerScene = SCNScene(named: "art.scnassets/meat/meat.scn")
                         if let markerNode = markerScene?.rootNode.childNodes.first{
-                                markerNode.eulerAngles.x = -.pi
-                                markerNode.opacity = 0
-                                markerNode.runAction(.fadeIn(duration: 1.3))
+                            markerNode.eulerAngles.x = -.pi
+                            markerNode.opacity = 0
+                            markerNode.runAction(.fadeIn(duration: 1.3))
+                            node.addChildNode(markerNode)
                         }
                         
-                   case "stark":
-                        print ("Rice marker detected")
+                    case "rice":
+                        print ("rice marker detected")
                         let appleScene = SCNScene(named: "art.scnassets/apple/apple.scn")
                         if let appleNode = appleScene?.rootNode.childNodes.first{
                             appleNode.eulerAngles.x = .pi / 2   
                             appleNode.position = SCNVector3(x:0, y:0.1, z:0)
-                            //planeNode.addChildNode(appleNode)
                             node.addChildNode(appleNode)
                         }
                         
                     case "mewtwo":
                         let markerScene = SCNScene(named: "art.scnassets/" + (nombreMarker)! + "/" + nombreMarker! + ".scn")
-//                        print("Mewtwo marker detected")
-//                        if let markerNode = markerScene?.rootNode.childNode(withName: nombreMarker!, recursively: true){
-//                            //Animacion para la aparicion
-//
-//                            let (min,max) = planeNode.boundingBox
-//                            let size = SCNVector3Make(max.x - min.x, max.y - min.y, max.z - min.z)
-//
-//                            let widthRatio = Float(imageAnchor.referenceImage.physicalSize.width)/size.x
-//                            let heightRatio = Float(imageAnchor.referenceImage.physicalSize.height)/size.z
-//
-//                            let finalRatio = [widthRatio,heightRatio].min()!
-//
-//                            planeNode.transform = SCNMatrix4(imageAnchor.transform)
-//                            planeNode.rotation = SCNVector4Make(0, 0, 0, 0)
-//
-//                            let aparicion = SCNAction.scale(to: CGFloat(finalRatio), duration: 0.8)
-//                            aparicion.timingMode = .easeOut
-//
-//                            planeNode.scale = SCNVector3Make(0, 0, 0)
-//                            planeNode.runAction(aparicion)
-//                            planeNode.addChildNode(markerNode)
-//                            //sceneView.scene.rootNode.addChildNode(markerNode)
-//                            sceneView.scene.rootNode.addChildNode(planeNode)
- //                       }
+                        print("Mewtwo marker detected")
+                        if let markerNode = markerScene?.rootNode.childNode(withName: nombreMarker!, recursively: true){
+                            markerNode.position = SCNVector3(x: 0, y: 0.01, z: 0)
+                            markerNode.name = nombreMarker
+                            node.addChildNode(markerNode)
+                            sceneView.scene.rootNode.addChildNode(node)
+                        }
                     default:
                         print("No existe referencia")
                     }
                 }
         }
+        node.name = "fromAnchor"
     return node
     }
-//
-//    @IBAction func showSizes(){
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let optionsVC = storyboard.instantiateViewController(withIdentifier: "itemOptionsViewController")
-//
-//        optionsVC.modalPresentationStyle = .popover
-//        //optionsVC.popoverPresentationController.
-//        //optionsVC.popoverPresentationController?.barButtonItem = Button
-//
-//        self.present(optionsVC,animated: true)
-//
-//    }
 }
